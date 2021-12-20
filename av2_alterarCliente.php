56d47146179ca3c45caa67176d0d95fa8419b4b2
lucassena7 <90392891+lucassena7@users.noreply.github.com>
2021-12-19 21:34:46 -0300

    Add files via upload

--- av2_alterarCliente.php
+++ av2_alterarCliente.php
@@ -10,31 +10,43 @@ $nomeBanco = "faeterj3dawav2"; //indicando o nome do banco
 //criando a conexão
 $conn = new mysqli($servidor, $usuario, $senha, $nomeBanco);
 if ($conn -> connect_error) //se a conexão der erro
-{
-	die ("conexão com erro: " .$conn->connect_error); //vai aparecer uma mensagem de erro dizendo qual foi este erro
-		 
-}
-if ($_SERVER["REQUEST_METHOD"] == "GET")
-	{	
-		$id = $_GET["id"];
-		
-		$comandoSQL = "SELECT * FROM `cliente` where id='$id' "; //comandoSQL será responsável por guardar a chamada que insere os dados vindo do formulário no banco
-		$result = $conn->query($comandoSQL); //Faça uma conexão através da conexão com o mysql, utilizando o metodo query que executa comandos. Result guardará o resultado do comandoSQL
-
-		$jCliente = json_encode($result , JSON_UNESCAPED_UNICODE);
-		echo $jCliente;
+	{
+		die ("conexão com erro: " .$conn->connect_error); //vai aparecer uma mensagem de erro dizendo qual foi este erro
+			 
 	}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET")
+	{
+		$id = $_GET["id"];
+			
+		$comandoSQL = "SELECT * FROM `cliente` where id='$id' "; //comandoSQL será responsável por guardar a chamada que insere os dados vindo do formulário no banco
+		//Como o cliete é unico, só buscará os dados de um cliente.
+		$result = $conn->query($comandoSQL); //Faça uma conexão através da conexão com o mysql, utilizando o metodo query que executa comandos. Result guardará o resultado do comandoSQL
+			
+		//$linha = $result->fetch_assoc(); //linha vai guardar os dados DO clientE
+			
+		$arrClientes = array();
+		$x=0;
+		while ($linha = $result->fetch_assoc()) 
+			{
+				$arrClientes [$x] = $linha;
+				$x++;
+			}
+			
+		$jCliente = json_encode($arrClientes, JSON_UNESCAPED_UNICODE);
+		echo $jCliente;
+		}
 	
 elseif($_SERVER["REQUEST_METHOD"] == "POST")
 	{
-		$nome  = $_GET["nome"];
-		$endereco = $_GET["endereco"];
-		$pcode = $_GET["pcode"];
-		$pais  = $_GET["pais"];
-		$cpf = $_GET["cpf"];
-		$passaporte= $_GET["passaporte"];
-		$email = $_GET["email"];
-		$data= $_GET["data"];
+		$nome  = $_POST["nome"];
+		$endereco = $_POST["endereco"];
+		$pcode = $_POST["pcode"];
+		$pais  = $_POST["pais"];
+		$cpf = $_POST["cpf"];
+		$passaporte= $_POST["passaporte"];
+		$email = $_$_POST["email"];
+		$data= $_$_POST["data"];
 		
 		$comandoSQL = "UPDATE `cliente` SET `nome`='$nome',`endereco`='$endereco',`postalCode`='$pcode',`pais`='$pais',`cpf`='$cpf',`passaporte`='$passaporte',`email`='$email',`dataNascimento`='$data' WHERE id = $id";
 		$resultado = $conn->query($comandoSQL);
